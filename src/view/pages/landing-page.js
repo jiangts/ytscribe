162a6aac41682d8b4a229bb035e20a82fc61ddb2
Allan Jiang <origami789@gmail.com>
2020-11-28 23:03:35 -0800

    update starter template

--- src/view/pages/landing-page.js
+++ src/view/pages/landing-page.js
@@ -1,20 +1,11 @@
-import SampleComponent from '../components/sample-component';
 
 export default function() {
-    return {        
-        view() {
-            /* return m('div', [
-                m('h2', 'Congratulations, you made it!'),
-                m('p', 'You\'ve spun up your very first Mithril app :-)'),
-                m(SampleComponent),
-            ]); */
-            return (
-                <div>
-                    <h2>Congratulations, you made it!</h2>
-                    <p>You've spun up your very first Mithril app :-)</p>
-                    <SampleComponent />
-                </div>
-            );
-        },
-    };
+  return {
+    view() {
+      return m('div', [
+        m('h2', 'Congratulations, you made it!'),
+        m('p', 'You\'ve spun up your very first Mithril app :-)'),
+      ]);
+    },
+  };
 }
